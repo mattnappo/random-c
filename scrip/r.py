6479e9fe8b5784f5a456e62f89c4867b3cc0e4e5
Matt Nappo <bolt@Matts-MBP.fios-router.home>
2017-10-29 14:21:50 -0400

    Made py scripts and learned lists

---
+++ scrip/r.py
@@ -0,0 +1,3 @@
+import os, sys
+stripped = sys.argv[1][:-4]
+os.system("../bin/" + stripped)
---
+++ src/diamonds.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+  int n = 1;
+  int c = 1;
+  int k = 1;
+  int space = 1;
+
+  cout<<"Enter an integer: ";
+  cin>>n;
+
+  space = n - 1;
+  for (k = 1; k <= n; k++) {
+    for (c = 1; c<=space; c++)
+       cout<<"   ";
+     space--;
+     for (c = 1; c<= 2*k-1; c++)
+       cout<<"[+]";
+     cout<<"\n";
+   }
+   space = 1;
+   for (k = 1; k<= n - 1; k++) {
+     for (c = 1; c<= space; c++)
+       cout<<"   ";
+     space++;
+     for (c = 1 ; c<= 2*(n-k)-1; c++)
+       cout<<"[+]";
+
+     cout<<"\n";
+   }
+}
