c66e3d80c4f9b60f18ac8342ac80e51afe65f028
xoreo <mattnappo@gmail.com>
2018-09-17 15:20:20 -0400

    working on cpp bin tree

--- binary_tree/cpp_binary_tree.cpp
+++ binary_tree/cpp_binary_tree.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <iostream>
 
-struct Node {
+struct node {
     int value;
-    Node *next;
+    struct node *right_child;
+    struct node *left_child;
 };
-
 
 class Tree {
 public:
@@ -18,7 +19,25 @@ public:
 };
 
 int main() {
-    
-    
+
+    struct node head;
+    head.value = 10;
+
+    struct node right_child;
+    right_child.value = 15;
+
+    struct node left_child;
+    left_child.value = 5;
+
+    std::cout << right_child.value << std::endl;
+    std::cout << left_child.value << std::endl;
+
+    // head.right_child = right_child;
+    // head.left_child = left_child;
+
+    // std::cout << head.value << std::endl;
+    // std::cout << head.right_child->value << std::endl;
+    // std::cout << head.left_child->value << std::endl;
+
     return 0;
 }
