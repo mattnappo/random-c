4e5e9a30bcb22088921692a3b6a8b7c20290511c
xoreo <mattnappo@gmail.com>
2018-09-17 18:32:16 -0400

    wrote print recursive func

--- binary_tree/cpp_binary_tree.cpp
+++ binary_tree/cpp_binary_tree.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iostream>
 
 class Node {
 private:
@@ -7,12 +6,36 @@ private:
     Node *right_child;
     Node *left_child;
 public:
+    Node(int value = 0, Node *right_child = NULL, Node *left_child = NULL) {
+        this->value = value;
+        this->right_child = right_child;
+        this->left_child = left_child;
+    }
+
+    void print_r(Node *node) {
+        node->print_node();
+        if (node->right_child != NULL) {
+            print_r(node->right_child);
+        }
+
+        if (node->left_child != NULL) {
+            print_r(node->left_child);
+        }
+
+    }
+
+    void print_node() {
+        std::cout << "Value: " << value << std::endl;
+    }
+
     void set_value(int value) {
         this->value = value;
     }
+
     void set_right_child(Node *right_child) {
         this->right_child = right_child;
     }
+
     void set_left_child(Node *left_child) {
         this->left_child = left_child;
     }
@@ -20,9 +43,11 @@ public:
     int get_value() {
         return value;
     }
+
     Node *get_right_child() {
         return right_child;
     }
+
     Node *get_left_child() {
         return left_child;
     }
@@ -41,6 +66,20 @@ public:
 };
 
 int main() {
-    
+
+    Node *head = new Node(10);
+
+    Node *right_child = new Node(20);
+    Node *left_child = new Node(5);
+
+    head->set_right_child(right_child);
+    head->set_left_child(left_child);
+
+    // head->print_node();
+    // head->get_left_child()->print_node();
+    // head->get_right_child()->print_node();
+
+    head->print_r(head);
+
     return 0;
 }
