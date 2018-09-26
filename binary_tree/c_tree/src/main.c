c670e15c8d00312d17331b853e0a270e4b676fc6
xoreo <mattnappo@gmail.com>
2018-09-25 23:27:10 -0400

    reset

--- binary_tree/c_tree/src/main.c
+++ binary_tree/c_tree/src/main.c
@@ -13,24 +13,14 @@ struct node {
     struct node *right_child;
 };
 
-struct node *new_node(int value) {
-    struct node *m_node;
-    m_node->value = value;
-    return m_node;
-}
 int main() {
     struct node *m_node;
-    
     m_node->value = 10;
-    m_node->right_child = new_node(15);
-    m_node->left_child = new_node(5);
+    printf("%d\n", m_node->value); // WORKS FINE
     
-    printf("%d\n", m_node->value);
-
-
     struct tree *m_tree;
     m_tree->head = m_node;
-    // printf("%d\n", m_tree->head->value);
+    printf("%d\n", m_tree->head->value);
 
 
     return 0;
