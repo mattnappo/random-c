0b00d8a3f509ddffb90059ac8ecebd224f92631a
xoreo <mattnappo@gmail.com>
2018-09-27 15:33:52 -0400

    backup

--- mesh/src/main.c
+++ mesh/src/main.c
@@ -18,34 +18,25 @@ void print_point(struct Point p) {
 }
 
 
-int main() {
-    // struct Point A = {1, 0, 0};
-    // struct Point B = {0, 1, 3};
-    // struct Point C = {2, 3, 2};
+int test_point() {
+    struct Point A = {1, 0, 0};
+    struct Point B = {0, 1, 3};
+    struct Point C = {2, 3, 2};
 
-    struct Point A = {0, 0, 0};
-    struct Point B = {3, 0, 0};
-    struct Point C = {3, 4, 0};
-
+    print_point(A);
+    print_point(B);
+    print_point(C);
+    
     float distances[] = {
         distance(A, B),
         distance(B, C),
         distance(A, C)
     };
 
-    print_point(A);
-    print_point(B);
-    print_point(C);
-
     int size = sizeof(distances) / sizeof(float);
     for (int i = 0; i < size; i++) {
         printf("%d: %f\n", i, distances[i]);
     }
-
-    
-    // print(a, b, c)
-    // print(a.distance(b)) #Should equal 3
-    // print(a.distance(c)) #Should equal 5
-    // print(b.distance(c)) #Should equal 4
-
 }
+
+int main() { test_point(); return 0; }
