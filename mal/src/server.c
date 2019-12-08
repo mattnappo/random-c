554b29a3ac0896e4a7cf595af1e1336b34733d22
xoreo <mattnappo@gmail.com>
2019-12-07 23:24:32 -0500

    working.

--- mal/src/server.c
+++ mal/src/server.c
@@ -1,22 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <unistd.h>
 
-#define MAX_CLIENTS 5
+#define MAX_CONN 5
+#define PORT 42001
+#define BUFF_SIZE 512
 
-int main()
+typedef struct server_reader_ {
+    int socket;
+} server_reader_;
+
+void *server_reader(void *argsp)
 {
-    int port = 9090;
+    server_reader_ *args = argsp;
+    int socket = args->socket;
+
+    while (1) {
+        char *buffer = malloc(BUFF_SIZE);
+
+        recv(socket, buffer, BUFF_SIZE, 0);
+        printf("[%s]\n", buffer);
+    }
+}
+
+int handle_conn(int socket)
+{
+    server_reader_ *args = malloc(sizeof(server_reader_ *));
+    args->socket = socket;
     
+    pthread_t reader_id;
+    pthread_create(&reader_id, NULL, server_reader, args);
+
+    return 0;
+}
+
+int run_server()
+{
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_address;
     server_address.sin_family = INADDR_ANY;
-    server_address.sin_port = htons(port);
+    server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     bind(
@@ -28,7 +58,31 @@ int main()
     int client_count = 0;
 
     printf("awaiting connections\n");
-    listen(server_socket, MAX_CLIENTS);
+    int status = listen(server_socket, MAX_CONN);
+    if (status != 0) {
+        printf("error listening on socket.");
+        return status;
+    }
+
+    while (client_count < MAX_CONN) {
+        int accepted_socket = accept(server_socket, NULL, NULL);
+
+        if (accepted_socket == -1) {
+            printf("unable to accept client\n");
+            return accepted_socket;
+        }
+
+        handle_conn(accepted_socket);
+    }
+
+    close(server_socket);
+
+    return 0;
+}
+
+int main()
+{
+    run_server();
 
     return 0;
 }
